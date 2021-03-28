44d2b9aa516265666feccd6e3b55bb6bdb978a64
Hebert Ryan <hebertryann40@gmail.com>
2021-03-28 00:29:24 -0300

    Update styles.ts

--- src/Pages/Chat/styles.ts
+++ src/Pages/Chat/styles.ts
@@ -73,7 +73,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 100vh;
+
 `;
 
 export const ChatContainer = styled.div`
