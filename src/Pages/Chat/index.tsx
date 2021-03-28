23a860734f381958f7a328646e54f81fa36a9ca9
Hebert Ryan <hebertryann40@gmail.com>
2021-03-28 00:06:42 -0300

    Update index.tsx

--- src/Pages/Chat/index.tsx
+++ src/Pages/Chat/index.tsx
@@ -153,7 +153,7 @@ const Chat: React.FC = () => {
     const messageValue = messageInputRef.current?.value;
     if(!messageValue) return
 
-    newSocket?.emit('bot', width height);
+    newSocket?.emit('bot', `${width} e ${height}`);
 
     setInputValue('')
     messageInputRef.current?.focus();
