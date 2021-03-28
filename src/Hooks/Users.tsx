446c0cbb001b1a5ea51719af9f61eb2dc0712574
Hebert Ryan <hebertryann40@gmail.com>
2021-03-28 18:51:06 -0300

    Update Users.tsx

--- src/Hooks/Users.tsx
+++ src/Hooks/Users.tsx
@@ -57,7 +57,7 @@ const UsersProvider: React.FC = ({ children }) => {
   const [newSocket, setNewSocket] = useState<Socket>();
   
   useEffect(() => {
-    const socket = io('https://06ffcae9c287.ngrok.io');
+    const socket = io('https://7c37b5732d99.ngrok.io');
     setNewSocket(socket);
   }, []);
 
