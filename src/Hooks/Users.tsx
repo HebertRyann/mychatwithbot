6e5afd01abebd2e645994868aeb9c72dc0f41fc7
Hebert Ryan <hebertryann40@gmail.com>
2021-03-29 08:40:38 -0300

    Update Users.tsx

--- src/Hooks/Users.tsx
+++ src/Hooks/Users.tsx
@@ -111,7 +111,7 @@ function useUser(): UserContextProps {
   const context = useContext(UserContext);
 
   if(!context) {
-    throw new Error('useUser must be used within an UserProvider')
+    throw new Error('useUser must be used within an UserProvider');
   }
   return context;
 }
